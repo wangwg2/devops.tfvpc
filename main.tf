8515fa45becf177db4acae399448145d349413f5
wangwg <wangwg2@msn.com>
2019-12-09 14:47:04 +0800

    Initial tfvpc moddule

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ resource "aws_vpc" "tfvpc" {
 }
 
 resource "aws_internet_gateway" "tfvpc" {
-  vpc_id = aws_vpc.tfb.id
+  vpc_id = aws_vpc.tfvpc.id
   tags = {
     Name = "${var.name}-igw"
   }
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.tfb.main_route_table_id
+  route_table_id         = aws_vpc.tfvpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.tfb.id
+  gateway_id             = aws_internet_gateway.tfvpc.id
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.tfb.id
+  vpc_id                  = aws_vpc.tfvpc.id
   cidr_block              = var.public_subnet
   map_public_ip_on_launch = var.map_public_ip_on_launch
   tags = {
